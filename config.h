666581cd361944cd62999c73588cd69d42da1a41
sircoolio <ericklein@users.noreply.github.com>
2025-08-28 13:05:04 -0700

    fixed status bug in sensorCO2Read()
    Fixed return bug in sensorCO2Read() when HARDWARE_SIMULATE enabled. Refactored sensor and WiFi initialization in powered_air_quality.ino to always include relevant code, regardless of simulation mode.

--- config.h
+++ config.h
@@ -7,16 +7,16 @@
 
 // Configuration Step 2: Set debug message output
 // comment out to turn off; 1 = summary, 2 = verbose
-// #define DEBUG 2
+#define DEBUG 2
 
 // Configuration Step 3: Simulate WiFi and sensor hardware, returning random but plausible values.
 // Comment out to turn off
-// #define HARDWARE_SIMULATE
+#define HARDWARE_SIMULATE
 
 // Configuration Step 4: Set network data endpoints
 // #define MQTT     // log sensor data to MQTT broker
 // #define HASSIO_MQTT  // And, if MQTT enabled, with Home Assistant too?
-#define INFLUX // Log data to InfluxDB server
+// #define INFLUX // Log data to InfluxDB server
 // #define THINGSPEAK  // Log data to ThingSpeak
 
 // Configuration Step 5: Which sensor configuration do we have?  Later generation devices
